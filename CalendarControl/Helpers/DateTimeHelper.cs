6350ee88362eadb0932772630aeb3409f8724083
martin.bosgra <martin.bosgra@protonmail.com>
2022-02-19 14:58:26 +0100

    Added method for weekend

--- CalendarControl/Helpers/DateTimeHelper.cs
+++ CalendarControl/Helpers/DateTimeHelper.cs
@@ -8,6 +8,25 @@ namespace CalendarControl.Helpers;
 /// </summary>
 internal static class DateTimeHelper
 {
+	/// <summary>
+	/// The weekday/weekend state for a given day.
+	/// </summary>
+	public enum DayState
+	{
+		/// <summary>
+		/// A work day.
+		/// </summary>
+		WorkDay,
+		/// <summary>
+		/// A weekend.
+		/// </summary>
+		Weekend,
+		/// <summary>
+		/// Morning is a workday, afternoon is the start of the weekend.
+		/// </summary>
+		WorkdayMorning
+	}
+
     /// <summary>
     /// Gets the begin of the week
     /// </summary>
@@ -65,4 +84,65 @@ internal static class DateTimeHelper
     /// <param name="dayOfWeek">Day of the week to convert</param>
     /// <returns>The string</returns>
     public static string DayOfWeekToString(DayOfWeek dayOfWeek) => GetCurrentDateFormat().DayNames[(int)dayOfWeek];
+
+	/// <summary>
+	/// Return if the passed in day of the week is a weekend. note: state pulled from http://en.wikipedia.org/wiki/Workweek_and_weekend
+	/// </summary>
+	/// <param name="obj">The CultureInfo this object.</param>
+	/// <param name="dayOfWeek">The Day of the week to return the state of.</param>
+	/// <returns>The weekday/weekend state of the passed in day of the week.</returns>
+	public static DayState GetDayState(CultureInfo obj, DayOfWeek dayOfWeek)
+	{
+		var items = obj.Name.Split(new[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
+		if (items.Length < 1) return default;
+		switch (items[^1])
+		{
+			case "DZ": // Algeria
+			case "BH": // Bahrain
+			case "BD": // Bangladesh
+			case "EG": // Egypt
+			case "IQ": // Iraq
+			case "IL": // Israel
+			case "JO": // Jordan
+			case "KW": // Kuwait
+			case "LY": // Libya
+			// Northern Malaysia (only in the states of Kelantan, Terengganu, and Kedah)
+			case "MV": // Maldives
+			case "MR": // Mauritania
+			case "NP": // Nepal
+			case "OM": // Oman
+			case "QA": // Qatar
+			case "SA": // Saudi Arabia
+			case "SD": // Sudan
+			case "SY": // Syria
+			case "AE": // U.A.E.
+			case "YE": // Yemen
+				return dayOfWeek is DayOfWeek.Thursday or DayOfWeek.Friday
+					? DayState.Weekend
+					: DayState.WorkDay;
+
+			case "AF": // Afghanistan
+			case "IR": // Iran
+				if (dayOfWeek == DayOfWeek.Thursday)
+					return DayState.WorkdayMorning;
+				return dayOfWeek == DayOfWeek.Friday ? DayState.Weekend : DayState.WorkDay;
+
+			case "BN": // Brunei Darussalam
+				return dayOfWeek is DayOfWeek.Friday or DayOfWeek.Sunday
+					? DayState.Weekend
+					: DayState.WorkDay;
+
+			case "MX": // Mexico
+			case "TH": // Thailand
+				if (dayOfWeek == DayOfWeek.Saturday)
+					return DayState.WorkdayMorning;
+				return dayOfWeek == DayOfWeek.Sunday
+					? DayState.Weekend
+					: DayState.WorkDay;
+
+		}
+
+		// most common Saturday/Sunday
+		return dayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday ? DayState.Weekend : DayState.WorkDay;
+	}
 }
