8444a4975aa7d497250e748b9c03b6e5cca5bfac
martin.bosgra <martin.bosgra@protonmail.com>
2022-03-11 17:29:08 +0100

    Small update

--- SatialInterfaces/Helpers/GeometryHelper.cs
+++ SatialInterfaces/Helpers/GeometryHelper.cs
@@ -15,8 +15,9 @@ public static class GeometryHelper
 	/// <returns>True in there and false otherwise</returns>
 	public static bool IsInRect(Vector v, Rect rect)
 	{
-		var xInRect = v.X >= rect.Left && v.X < rect.Right;
-		var yInRect = v.Y >= rect.Top && v.Y < rect.Bottom;
+		var (x, y) = v;
+		var xInRect = x >= rect.Left && x < rect.Right;
+		var yInRect = y >= rect.Top && y < rect.Bottom;
 		return xInRect && yInRect;
 	}
 }
